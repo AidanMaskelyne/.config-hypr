e0193a189b1e6043ba55275c21a1740685ac454c
Aidan M <aidan@muttleyville.org>
2024-01-14 20:53:28 +0000

    Init

---
+++ scripts/lock.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+swaylock \
+	--hide-keyboard-layout \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--ring-color ffffff15 \
+	--ring-ver-color 19cb00 \
+	--ring-wrong-color cc0403 \
+	--ring-clear-color cc0403 \
+	--key-hl-color 0078d4 \
+	--bs-hl-color cc0403 \
+	--text-color ffffff \
+	--text-caps-lock-color ffffff \
+	--line-color 00000000 \
+	--line-ver-color 00000000 \
+	--line-wrong-color 00000000 \
+	--line-clear-color 00000000 \
+	--separator-color 00000000 \
+	--inside-color 000000 \
+	--inside-ver-color 19cb00 \
+	--inside-wrong-color cc0403 \
+	--inside-clear-color cc0403 \
+	--color 000000 \
+	--screenshots \
+	--effect-blur 7x5 \
+	--grace 5 \
+	--clock \
+	--indicator
---
+++ scripts/logout.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wlogout --protocol layer-shell -b 4 -T 400 -B 400
